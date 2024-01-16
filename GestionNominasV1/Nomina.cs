79e8577a49ab4d854fcc760a02766c5cc03f7dc6
anabelmorenogomez <amorenogomez5@hotmail.com>
2024-01-16 09:32:37 +0100

    Creación de la propiedad NombreEmpleado

--- GestionNominasV1/Nomina.cs
+++ GestionNominasV1/Nomina.cs
@@ -14,5 +14,28 @@ namespace GestionNominasV1
         private string _puesto;
         private int _horasTrabajadas;
         private float _salariohora;
+
+        // Propiedades
+        /// <summary>
+        /// Nombre del Empleado 
+        /// </summary>
+        public string NombreEmpelado
+        {
+            get {
+                // Control de contenido 
+                if (string.IsNullOrEmpty(_nombre)) throw new Exception("Nombre del trabajador no establecido");
+                // Devolucion del valor 
+            return _nombre;
+            }
+            set {
+                // Validación de los datos a establecer 
+                if (string.IsNullOrEmpty(value)) throw new Exception("El nombre del trabjador no puede ser null o la cadena vacía");
+                {
+                    
+                }
+                // Establecimiento del valor al miembro privado 
+                _nombre = value;
+            }
+        }
     }
 }
